d66d4c17da02bb9ea36ebcb4c0109d23e7c2e46d
villarmeaels <34365402+villarmeaels@users.noreply.github.com>
2018-01-31 10:06:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body bgcolor="grey">
+	<a href="start.png">next</a>
+	<a href="dictionary.png">next</a>
+	<a href="recent.png">next</a>
+	<a href="sidemenu.png">next</a>
+	<a href="wordfortheday.png">next</a>
+	<a href="weballwords.png">next</a>
+	<a href="webdic.png">next</a>
+	<a href="webrecentsearch.png">next</a>
+	<a href="webwordfortheday.png">next</a>
+	<a href="serveradd.png">next</a>
+	<a href="serverallwords.png">next</a>
+	<a href="serverviewsearch.png">next</a>
+	<a href="serveraddwordfortheday.png">next</a>
+	<a href="serverviewwordfortheday.png">next</a>
+</body>
+</html>
