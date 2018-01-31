f3be799f5baec7f5b081b59dcab58681236bbe45
villarmeaels <34365402+villarmeaels@users.noreply.github.com>
2018-01-31 10:25:08 +0800

    Add files via upload

---
+++ serveraddwordfortheday.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<a href="serveradd.php">previous</a>
+	<a href="serveraddwordfortheday.php">next</a>
+
+</body>
+</html>
---
+++ webrecent.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<a href="webdictionary.php">previous</a>
+	<a href="webrecent.php">next</a>
+
+</body>
+</html>
