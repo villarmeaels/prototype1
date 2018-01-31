28c48f692ff3d91e705dac2bc13c8a3f3f04fb83
villarmeaels <34365402+villarmeaels@users.noreply.github.com>
2018-01-31 11:49:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<img src = "start.png">
+	<img src = "dictionary.png">
+	<img src = "recent.png">
+	<img src = "dialect.png">
+	<img src = "wordoftheday.png">
+	<img src = "sidemenu.png">
+	<img src = "webdic.png">
+	<img src = "webrecentsearch.png">
+	<img src = "weballwords.png">
+	<img src = "webwordfortheday.png">
+	<img src = "serveradd.png">
+	<img src = "serveraddwordfortheday.png">
+	<img src = "serverallwords.png">
+	<img src = "serverviewsearch.png">
+	<img src = "serverviewwordfortheday.png">
+</body>
+</html>
